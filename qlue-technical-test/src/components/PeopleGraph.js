4618118130d5426d624adb355bee885b2538b267
maulana_as <maulanaadly05@gmail.com>
2021-03-28 10:56:53 +0700

    chart people

--- qlue-technical-test/src/components/PeopleGraph.js
+++ qlue-technical-test/src/components/PeopleGraph.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Chart from './chart';
+import './index.css';
 
-const PeopleGraph = () => { 
 
-    return (
-        <div>
-            <h1>Hallo Graph People</h1>
+function PeopleGraph() { 
+    
+    const [stateData, setStateData] = useState({});
+
+    useEffect(() => { 
+       async function getData ()  {
+           const res = await fetch(`https://swapi.dev/api/people`)
+           const data = await res.json();
+           setStateData(data);
+       };
+       getData();
+    }, []);
+
+    
+    return ( 
+        <div> 
+            <Chart data={stateData} />
         </div>
     )
+
 }
 
 export default PeopleGraph;
