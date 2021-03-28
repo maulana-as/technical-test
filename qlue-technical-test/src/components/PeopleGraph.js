e354da0befc8df7bb8cf31a57ef7dbfdfa3745fd
maulana_as <maulanaadly05@gmail.com>
2021-03-28 16:12:01 +0700

    delete chart.js component

--- qlue-technical-test/src/components/PeopleGraph.js
+++ qlue-technical-test/src/components/PeopleGraph.js
@@ -74,42 +74,5 @@ const PeopleGraph = () => {
     </>
   );
 }
-    //   const getData = async () => {
-//     try {
-//       const res = await axios.get(
-//         `https://swapi.dev/api/people`
-//       );
-
-//       setChart({
-//         labels: [Object.keys(res.data.timeline.cases)],
-//         datasets: [
-//           {
-//             label: "Data People",
-//             fill: false,
-//             lineTension: 0.1,
-//             backgroundColor: "rgba(75,192,192,0.4)",
-//             borderColor: "rgba(75,192,192,1)",
-//             borderCapStyle: "butt",
-//             borderDash: [],
-//             borderDashOffset: 0.0,
-//             borderJoinStyle: "miter",
-//             pointBorderColor: "rgba(75,192,192,1)",
-//             pointBackgroundColor: "#fff",
-//             pointBorderWidth: 1,
-//             pointHoverRadius: 5,
-//             pointHoverBackgroundColor: "rgba(75,192,192,1)",
-//             pointHoverBorderColor: "rgba(220,220,220,1)",
-//             pointHoverBorderWidth: 2,
-//             pointRadius: 1,
-//             pointHitRadius: 10,
-//             data: Object.values(res.data.timeline.cases)
-//           }
-//         ]
-//       });
-//     } catch (error) {
-//       console.log(error.response);
-//     }
-//   };
-
 
 export default PeopleGraph;
--- qlue-technical-test/src/components/chart.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Line } from "react-chartjs-2";
-
-const Chart = ({ data }) => {
-  
-  return <Line data={data} options={{ responsive: true, height: '600px', width: "600px" }} />;
-};
-
-export default Chart;
